67dc7e12b933d529725c8e4b7e7b8ec21bbbf4be
JesperRU <MrJesper@yandex.ru>
2017-11-30 13:54:32 +0600

    Backend rewriting started. Controller is main class for application logic.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,11 +5,10 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Slider;
+import javafx.geometry.Side;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -22,13 +21,18 @@ import java.util.List;
 
 
 public class Controller {
+    private double VIEW_HEIGHT = 60;
+    private double VIEW_WIDTH = 500;
+    private double UNSELECTED_X = 50;
+    private double UNSELECTED_Y_OFFSET = 10;
+
 
     @FXML
     public Label timeElapsed;
     @FXML
     public Label timeTotal;
     @FXML
-    public ListView<String> list;
+    public ProgressBar volumeBar;
     @FXML
     public Slider volumeSlider;
     @FXML
@@ -52,103 +56,104 @@ public class Controller {
 
     private ArrayList<Track> loaded;
 
-    private ArrayList<Track> selected;
+    private ArrayList<TrackView> selected;
 
     private Track currentTrack;
 
+    private void addView(Track track) {
 
-    private void addView(final Track track)
-    {
-        if (views == null) views = new ArrayList<>();
-        TrackView view = new TrackView(track, views, viewContainer);
+        TrackView view = new TrackView(track);
+        view.getView().setPrefSize(VIEW_WIDTH,VIEW_HEIGHT);
+        view.getView().setId("libNode");
+        view.getView().setLayoutX(UNSELECTED_X);
+        if (views.size() == 0)
+            view.getView().setLayoutY(10 + views.size()*VIEW_HEIGHT);
+        else
+            view.getView().setLayoutY(views.get(views.size() - 1).getView().getLayoutY() + 10 + VIEW_HEIGHT*views.size());
+
+        track.getPlayer().setOnReady(() -> {
+            track.titleProperty().set(track.getMedia().getMetadata().get("title").toString());
+            track.artistProperty().set(track.getMedia().getMetadata().get("artist").toString());
+
+            if (track.getMedia().getMetadata().get("album") == null) {
+                track.albumProperty().set("Unknown Album");
+            }else {
+                track.albumProperty().set(track.getMedia().getMetadata().get("album").toString());
+            }
+
+        });
+
+
+        ContextMenu menu = new ContextMenu();
+        MenuItem deleteItem = new MenuItem();
+        deleteItem.setText("Delete");
+        deleteItem.setOnAction(event -> viewContainer.getChildren().remove(view.getView()));
+        menu.getItems().add(deleteItem);
+
+
+        view.getView().setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton() == MouseButton.SECONDARY) {
+                    menu.show(view.getView(), Side.BOTTOM, 0, 0);
+                } else if (!event.isShiftDown() && event.getButton() == MouseButton.PRIMARY) {
+                    for (TrackView view : views) {
+                        view.getView().setId("libNode");
+                    }
+                    selected.clear();
+                }
+                if (event.getButton() == MouseButton.PRIMARY)
+                {
+                    selected.add(view);
+                    view.getView().setId("selectedNode");
+
+                    if (event.getClickCount() == 2)
+                    {
+                        if (currentTrack != null) currentTrack.stop();
+                        currentTrack = view.getTrack();
+                        playClick();
+                    }
+
+                }
+            }
+        });
+        Label metadata = new Label();
+        metadata.setLayoutX(20);
+        metadata.setLayoutY(5);
+        metadata.setId("label");
+        metadata.setMaxWidth(200);
+        view.getView().getChildren().add(metadata);
+        metadata.textProperty().bind(track.artistProperty());
+
+
+        metadata = new Label();
+        metadata.setLayoutX((VIEW_WIDTH / 100 * 65)); metadata.setLayoutY(10);
+        metadata.setId("label");
+        metadata.setMaxWidth(200);
+        metadata.textProperty().bind(track.titleProperty());
+        view.getView().getChildren().add(metadata);
+
+
+        metadata = new Label();
+        metadata.setLayoutX(100); metadata.setLayoutY(30);
+        metadata.setId("label");
+        metadata.setMaxWidth(200);
+        metadata.textProperty().bind(track.albumProperty());
+        view.getView().getChildren().add(metadata);
+        view.getView().applyCss();
         viewContainer.getChildren().add(view.getView());
+        views.add(view);
+
     }
 
 
     @FXML
     protected void initialize() {
-
-        pauseButton.setVisible(false);
-        songSlider.setMin(0);
-        songSlider.setBlockIncrement(1);
-        songSlider.setValue(0);
-        songSlider.setMax(100);
-        songBar.setProgress(0);
-        songSlider.setValue(50);
-
-        songSlider.valueProperty().addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable observable) {
-                if (currentTrack!= null) songBar.setProgress(songSlider.getValue()/currentTrack.getPlayer().getCurrentTime().toSeconds());
-                else
-                {
-                    songBar.setProgress(songSlider.getValue() / 100);
-                }
-            }
-        });
+        views = new ArrayList<>();
+        loaded = new ArrayList<>();
+        selected = new ArrayList<>();
 
 
-        playButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                playClick(null);
-            }
-        });
-
-
-        volumeSlider.valueProperty().addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable observable) {
-
-            }
-        });
-
-        loaded = new ArrayList<Track>();
-
-
-
-        volumeSlider.setMax(100);
-        volumeSlider.setMin(0);
-        volumeSlider.setBlockIncrement(1);
-        volumeSlider.setValue(50);
-
-        list.setVisible(false);
-        list.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                String selectedItem = list.getSelectionModel().getSelectedItem();
-
-                if (event.getClickCount() == 2) {
-                    for (Track load : loaded) {
-                        if (load.getSource().getName().equals(selectedItem)) {
-                            if (currentTrack != null) currentTrack.stop();
-                            //selected = load;
-                            //currentTrack = load;
-                            playClick(null);
-                        }
-                    }
-
-
-                } else if (event.getClickCount() == 1) {
-                    for (Track load : loaded) {
-                        if (load.getSource().getName().equals(selectedItem)) {
-                            if (currentTrack != load) {
-                                playButton.setVisible(true);
-                                pauseButton.setVisible(false);
-                            } else {
-                                playButton.setVisible(false);
-                                pauseButton.setVisible(true);
-                            }
-                            // selected = load;
-                        }
-
-                    }
-
-                }
-            }
-
-
-        });
     }
 
     public void browseClick() {
@@ -159,19 +164,15 @@ public class Controller {
         FileChooser.ExtensionFilter wavFilter = new FileChooser.ExtensionFilter("Wav (*.wav)", "*.wav");
         browser.getExtensionFilters().add(mp3Filter);
         browser.getExtensionFilters().add(wavFilter);
+        List<File> selected = browser.showOpenMultipleDialog(pane.getScene().getWindow());
+        if (selected == null) return;
 
-
-        List<File> selections = browser.showOpenMultipleDialog(playButton.getScene().getWindow());
-
-        if (selections.size() > 0) {
-                for (File selected : selections) {
-                    final Track selectedTrack = new Track(selected);
-                    addView(selectedTrack);
-                    loaded.add(selectedTrack);
-                }
-
+        for(File selectedFile : selected)
+        {
+            Track selectedTrack = new Track(selectedFile);
+            addView(selectedTrack);
+            loaded.add(selectedTrack);
         }
-
 
     }
 
@@ -180,90 +181,24 @@ public class Controller {
     private InvalidationListener volumeSliderInvalidationListener;
 
 
-    public void playClick(ActionEvent actionEvent) {
-        if (selected == null && currentTrack == null) return;
+    public void playClick() {
+        if (selected.size() > 0) return;
 
-        if (currentTrack != null) currentTrack.stop();
-
-        playButton.setVisible(false);
-        pauseButton.setVisible(true);
-        pauseButton.requestFocus();
-
-        String selectedItem = list.getSelectionModel().getSelectedItem();
-        if (selectedItem != null) {
-            for (Track track : loaded) {
-                if (track.getSource().getName().equals(selectedItem)) {
-                    //selected = track;
-                    break;
-                }
-            }
-            if (selected != null) {
-                disposeCurrent();
-                // currentTrack = selected;
-                songSlider.setMax((int) currentTrack.getMedia().getDuration().toSeconds());
-
-
-                sliderValueUpdater = new ChangeListener<Duration>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
-                        songSlider.setValue((int) currentTrack.getPlayer().getCurrentTime().toSeconds());
-                        updateTimeLabelValue();
-                    }
-                };
-
-                currentTrack.getPlayer().currentTimeProperty().addListener(sliderValueUpdater);
-
-
-                currentTrack.getPlayer().setVolume(volumeSlider.getValue() / 100);
-
-                volumeSliderInvalidationListener  = new InvalidationListener() {
-                    @Override
-                    public void invalidated(Observable ov) {
-                        if (volumeSlider.isValueChanging()) {
-                            currentTrack.getPlayer().setVolume(volumeSlider.getValue() / 100);
-                        }
-                        updateVolumeSliderValue();
-                    }
-                };
-
-                volumeSlider.valueProperty().addListener(volumeSliderInvalidationListener);
-
-                songSliderInvalidationListener = new InvalidationListener() {
-                    @Override
-                    public void invalidated(Observable ov) {
-                        if (songSlider.isValueChanging()) {
-                            currentTrack.getPlayer().seek(Duration.seconds(Math.floor(songSlider.getValue())));
-                        }
-                    }
-                };
-                songSlider.valueProperty().addListener(songSliderInvalidationListener);
-                currentTrack.play();
-            }
-        }
     }
 
     public void disposeCurrent()
     {
-        if (currentTrack != null && sliderValueUpdater != null) currentTrack.getPlayer().currentTimeProperty().removeListener(sliderValueUpdater);
-        if (currentTrack != null && songSliderInvalidationListener != null) songSlider.valueProperty().removeListener(songSliderInvalidationListener);
-        if (currentTrack != null && volumeSliderInvalidationListener != null) volumeSlider.valueProperty().removeListener(volumeSliderInvalidationListener);
+        ///if (currentTrack != null && sliderValueUpdater != null) currentTrack.getPlayer().currentTimeProperty().removeListener(sliderValueUpdater);
+        /// if(currentTrack != null && songSliderInvalidationListener != null) songSlider.valueProperty().removeListener(songSliderInvalidationListener);
+        ///if (currentTrack != null && volumeSliderInvalidationListener != null) volumeSlider.valueProperty().removeListener(volumeSliderInvalidationListener);
     }
-    public void pauseClick(ActionEvent actionEvent) {
+
+
+    public void pauseClick() {
         playButton.setVisible(true);
         pauseButton.setVisible(false);
         playButton.requestFocus();
-        currentTrack.pause();
-
-
-    }
-
-    private void updateVolumeSliderValue() {
-        volumeSlider.setValue(currentTrack.getPlayer().getVolume() * 100);
-    }
-
-    private void updateTimeLabelValue() {
-        timeElapsed.setText(Integer.toString((int) currentTrack.getPlayer().getCurrentTime().toSeconds() / 60) + ":" + Integer.toString((int) currentTrack.getPlayer().getCurrentTime().toSeconds() % 60));
-        timeTotal.setText(Integer.toString((int) currentTrack.getMedia().getDuration().toSeconds() / 60) + ":" + Integer.toString((int) currentTrack.getMedia().getDuration().toSeconds() % 60));
+        //currentTrack.pause();
     }
 
 }
