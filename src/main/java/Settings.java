aaa3ea23661b646f681b3abbb9fcab4187606ec7
JesperRU <mrjesper@yandex.ru>
2017-12-06 16:39:16 +0600

    Added Icon to settings scene. Settings panel configured. Added xml serialization of settings objects.
    First version.
    TODO: May be singleton will be better here?
    
    Signed-off-by: Jesper <MrJesper@yandex.ru>

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -1,2 +1,65 @@
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
+
+//TODO: ADD SOME TESTS
 public class Settings {
+
+    public Settings()
+    {
+
+    }
+
+    public static Settings load() {
+        File settingsFile = new File("settings.xml");
+        Settings settings = null;
+        try (FileInputStream fis = new FileInputStream(settingsFile)){
+            XMLDecoder decoder = new XMLDecoder(fis);
+            settings = (Settings) decoder.readObject();
+            decoder.close();
+            fis.close();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return settings;
+    }
+    public static void save(Settings settings)
+    {
+        File settingsFile = new File("jplayer_settings.xml");
+        try {
+            if (!settingsFile.exists() && settingsFile.createNewFile())
+            {
+                FileOutputStream fos = new FileOutputStream(settingsFile);
+                XMLEncoder encoder = new XMLEncoder(fos);
+                encoder.writeObject(settings);
+                encoder.close();
+                fos.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int propertyOne;
+    private String propertyTwo;
+
+    public int getPropertyOne() {
+        return propertyOne;
+    }
+
+    public void setPropertyOne(int propertyOne) {
+        this.propertyOne = propertyOne;
+    }
+
+    public String getPropertyTwo() {
+        return propertyTwo;
+    }
+
+    public void setPropertyTwo(String propertyTwo) {
+        this.propertyTwo = propertyTwo;
+    }
 }
--- src/main/java/SettingsController.java
+++ src/main/java/SettingsController.java
@@ -1,2 +1,11 @@
+import javafx.fxml.FXML;
+
 public class SettingsController {
+    @FXML
+    protected void initialize() {
+        Settings settings = new Settings();
+        settings.setPropertyOne(5);
+        settings.setPropertyTwo("kek");
+        Settings.save(settings);
+    }
 }
